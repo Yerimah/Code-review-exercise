4377d074806a9e533cfb5f9fb4bfee59ed15697c
Yerimah <danielyerimah@yahoo.com>
2022-04-29 17:07:31 +0100

    fix linter error

--- src/function.js
+++ src/function.js
@@ -1,9 +1,9 @@
 export default function showAllTodo(tasks) {
-    const listElement = document.querySelector('.list');
-    listElement.innerHTML = '';
-  
-    tasks.list.forEach((listItem) => {
-      listElement.innerHTML += `
+  const listElement = document.querySelector('.list');
+  listElement.innerHTML = '';
+
+  tasks.list.forEach((listItem) => {
+    listElement.innerHTML += `
           <div class="row" id="${listItem.index}">
               <div class="input">
               <input type="checkbox" class="check" ${listItem.completed ? 'checked' : ''}>
@@ -16,44 +16,44 @@ export default function showAllTodo(tasks) {
           </div>
           
         `;
+  });
+
+  document.querySelectorAll('.list-text').forEach((inp) => {
+    inp.addEventListener('click', () => {
+      inp.readOnly = false;
+      inp.focus();
+      inp.parentNode.parentNode.querySelector('.fa-trash-alt').classList.remove('hidden');
+      inp.parentNode.parentNode.querySelector('.fa-ellipsis-v').classList.add('hidden');
     });
-  
-    document.querySelectorAll('.list-text').forEach((inp) => {
-      inp.addEventListener('click', () => {
-        inp.readOnly = false;
-        inp.focus();
-        inp.parentNode.parentNode.querySelector('.fa-trash-alt').classList.remove('hidden');
-        inp.parentNode.parentNode.querySelector('.fa-ellipsis-v').classList.add('hidden');
-      });
-      inp.addEventListener('change', () => {
-        const { id } = inp.parentNode.parentNode;
-        const currentTask = tasks.list.find((t) => t.index === +id);
-        currentTask.description = inp.value;
-  
-        tasks.updateTodo(currentTask);
-  
-        setTimeout(() => {
-          inp.parentNode.parentNode.querySelector('.fa-trash-alt').classList.add('hidden');
-          inp.parentNode.parentNode.querySelector('.fa-ellipsis-v').classList.remove('hidden');
-          inp.readOnly = true;
-        }, 200);
-      });
+    inp.addEventListener('change', () => {
+      const { id } = inp.parentNode.parentNode;
+      const currentTask = tasks.list.find((t) => t.index === +id);
+      currentTask.description = inp.value;
+
+      tasks.updateTodo(currentTask);
+
+      setTimeout(() => {
+        inp.parentNode.parentNode.querySelector('.fa-trash-alt').classList.add('hidden');
+        inp.parentNode.parentNode.querySelector('.fa-ellipsis-v').classList.remove('hidden');
+        inp.readOnly = true;
+      }, 200);
     });
-  
-    document.querySelectorAll('.fa-trash-alt').forEach((btn) => {
-      btn.addEventListener('click', () => {
-        const { id } = btn.parentNode.parentNode;
-        tasks.removeTodo(+id);
-        showAllTodo(tasks);
-      });
+  });
+
+  document.querySelectorAll('.fa-trash-alt').forEach((btn) => {
+    btn.addEventListener('click', () => {
+      const { id } = btn.parentNode.parentNode;
+      tasks.removeTodo(+id);
+      showAllTodo(tasks);
     });
-  
-    document.querySelectorAll('.check').forEach((box) => {
-      box.addEventListener('change', () => {
-        const { id } = box.parentNode.parentNode;
-        const currentTask = tasks.list.find((t) => t.index === +id);
-        currentTask.completed = box.checked;
-        tasks.updateTodo(currentTask);
-      });
+  });
+
+  document.querySelectorAll('.check').forEach((box) => {
+    box.addEventListener('change', () => {
+      const { id } = box.parentNode.parentNode;
+      const currentTask = tasks.list.find((t) => t.index === +id);
+      currentTask.completed = box.checked;
+      tasks.updateTodo(currentTask);
     });
-  }
+  });
+}
